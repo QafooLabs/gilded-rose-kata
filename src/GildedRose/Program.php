17f5c52e2317ea1eb65f28c9f54ce421353c9dca
Benjamin Eberlei <kontakt@beberlei.de>
2013-01-16 14:17:30 +0100

    Fix CS

--- src/GildedRose/Program.php
+++ src/GildedRose/Program.php
@@ -38,38 +38,26 @@ class Program
 
     public function UpdateQuality()
     {
-        for ($i = 0; $i < count($this->items); $i++)
-        {
-            if ($this->items[$i]->name != "Aged Brie" && $this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if ($this->items[$i]->quality > 0)
-                {
-                    if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros")
-                    {
+        for ($i = 0; $i < count($this->items); $i++) {
+            if ($this->items[$i]->name != "Aged Brie" && $this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
+                if ($this->items[$i]->quality > 0) {
+                    if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
                         $this->items[$i]->quality = $this->items[$i]->quality - 1;
                     }
                 }
-            }
-            else
-            {
-                if ($this->items[$i]->quality < 50)
-                {
+            } else {
+                if ($this->items[$i]->quality < 50) {
                     $this->items[$i]->quality = $this->items[$i]->quality + 1;
 
-                    if ($this->items[$i]->name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if ($this->items[$i]->sellIn < 11)
-                        {
-                            if ($this->items[$i]->quality < 50)
-                            {
+                    if ($this->items[$i]->name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if ($this->items[$i]->sellIn < 11) {
+                            if ($this->items[$i]->quality < 50) {
                                 $this->items[$i]->quality = $this->items[$i]->quality + 1;
                             }
                         }
 
-                        if ($this->items[$i]->sellIn < 6)
-                        {
-                            if ($this->items[$i]->quality < 50)
-                            {
+                        if ($this->items[$i]->sellIn < 6) {
+                            if ($this->items[$i]->quality < 50) {
                                 $this->items[$i]->quality = $this->items[$i]->quality + 1;
                             }
                         }
@@ -77,34 +65,23 @@ class Program
                 }
             }
 
-            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros")
-            {
+            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
                 $this->items[$i]->sellIn = $this->items[$i]->sellIn - 1;
             }
 
-            if ($this->items[$i]->sellIn < 0)
-            {
-                if ($this->items[$i]->name != "Aged Brie")
-                {
-                    if ($this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if ($this->items[$i]->quality > 0)
-                        {
-                            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros")
-                            {
+            if ($this->items[$i]->sellIn < 0) {
+                if ($this->items[$i]->name != "Aged Brie") {
+                    if ($this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if ($this->items[$i]->quality > 0) {
+                            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
                                 $this->items[$i]->quality = $this->items[$i]->quality - 1;
                             }
                         }
-                    }
-                    else
-                    {
+                    } else {
                         $this->items[$i]->quality = $this->items[$i]->quality - $this->items[$i]->quality;
                     }
-                }
-                else
-                {
-                    if ($this->items[$i]->quality < 50)
-                    {
+                } else {
+                    if ($this->items[$i]->quality < 50) {
                         $this->items[$i]->quality = $this->items[$i]->quality + 1;
                     }
                 }
