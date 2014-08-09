87f6ffb85893e335bec56259ba0b9d3e1b805f14
Ian Monge <ian.monge@socialpoint.es>
2014-08-09 21:12:18 +0200

    WIP

--- src/GildedRose/Program.php
+++ src/GildedRose/Program.php
@@ -49,30 +49,30 @@ class Program
 {
     private $items = array();
 
-    public static function Main()
-    {
-        echo "HELLO\n";
-
-        $app = new Program(array(
-              new Item(array( 'name' => "+5 Dexterity Vest",'sellIn' => 10,'quality' => 20)),
-              new Item(array( 'name' => "Aged Brie",'sellIn' => 2,'quality' => 0)),
-              new Item(array( 'name' => "Elixir of the Mongoose",'sellIn' => 5,'quality' => 7)),
-              new Item(array( 'name' => "Sulfuras, Hand of Ragnaros",'sellIn' => 0,'quality' => 80)),
-              new Item(array(
-                     'name' => "Backstage passes to a TAFKAL80ETC concert",
-                     'sellIn' => 15,
-                     'quality' => 20
-              )),
-              new Item(array('name' => "Conjured Mana Cake",'sellIn' => 3,'quality' => 6)),
-        ));
-
-        $app->UpdateQuality();
-
-        echo sprintf("%50s - %7s - %7s\n", "Name", "SellIn", "Quality");
-        foreach ($app->items as $item) {
-            echo sprintf("%50s - %7d - %7d\n", $item->name, $item->sellIn, $item->quality);
-        }
-    }
+//    public static function Main()
+//    {
+//        echo "HELLO\n";
+//
+//        $app = new Program(array(
+//              new Item(array( 'name' => "+5 Dexterity Vest",'sellIn' => 10,'quality' => 20)),
+//              new Item(array( 'name' => "Aged Brie",'sellIn' => 2,'quality' => 0)),
+//              new Item(array( 'name' => "Elixir of the Mongoose",'sellIn' => 5,'quality' => 7)),
+//              new Item(array( 'name' => "Sulfuras, Hand of Ragnaros",'sellIn' => 0,'quality' => 80)),
+//              new Item(array(
+//                     'name' => "Backstage passes to a TAFKAL80ETC concert",
+//                     'sellIn' => 15,
+//                     'quality' => 20
+//              )),
+//              new Item(array('name' => "Conjured Mana Cake",'sellIn' => 3,'quality' => 6)),
+//        ));
+//
+//        $app->UpdateQuality();
+//
+//        echo sprintf("%50s - %7s - %7s\n", "Name", "SellIn", "Quality");
+//        foreach ($app->items as $item) {
+//            echo sprintf("%50s - %7d - %7d\n", $item->name, $item->sellIn, $item->quality);
+//        }
+//    }
 
     public function __construct(array $items)
     {
@@ -82,53 +82,104 @@ class Program
     public function UpdateQuality()
     {
         for ($i = 0; $i < count($this->items); $i++) {
-            if ($this->items[$i]->name != "Aged Brie" && $this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
-                if ($this->items[$i]->quality > 0) {
-                    if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                        $this->items[$i]->quality = $this->items[$i]->quality - 1;
-                    }
-                }
-            } else {
-                if ($this->items[$i]->quality < 50) {
-                    $this->items[$i]->quality = $this->items[$i]->quality + 1;
+            $item = $this->items[$i];
+            $this->updateQualityOfItem($item);
+        }
+    }
 
-                    if ($this->items[$i]->name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if ($this->items[$i]->sellIn < 11) {
-                            if ($this->items[$i]->quality < 50) {
-                                $this->items[$i]->quality = $this->items[$i]->quality + 1;
-                            }
+    /**
+     * @return Item[]
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param Item $item
+     */
+    private function updateQualityOfItem(Item $item)
+    {
+        if (!$this->isSpecialItem($item) && $item->quality > 0) {
+            $item->quality = $item->quality - 1;
+        } else {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+
+                if ($this->isBackstagePassesItem($item)) {
+                    if ($item->sellIn < 11) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
-
-                        if ($this->items[$i]->sellIn < 6) {
-                            if ($this->items[$i]->quality < 50) {
-                                $this->items[$i]->quality = $this->items[$i]->quality + 1;
-                            }
-                        }
                     }
-                }
-            }
 
-            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                $this->items[$i]->sellIn = $this->items[$i]->sellIn - 1;
-            }
-
-            if ($this->items[$i]->sellIn < 0) {
-                if ($this->items[$i]->name != "Aged Brie") {
-                    if ($this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if ($this->items[$i]->quality > 0) {
-                            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                                $this->items[$i]->quality = $this->items[$i]->quality - 1;
-                            }
+                    if ($item->sellIn < 6) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
-                    } else {
-                        $this->items[$i]->quality = $this->items[$i]->quality - $this->items[$i]->quality;
-                    }
-                } else {
-                    if ($this->items[$i]->quality < 50) {
-                        $this->items[$i]->quality = $this->items[$i]->quality + 1;
                     }
                 }
             }
         }
+
+        if (!$this->isSulfurasItem($item)) {
+            $item->sellIn = $item->sellIn - 1;
+        }
+
+        if ($item->sellIn < 0) {
+            if (!$this->isAgedBrieItem($item)) {
+                if (!$this->isBackstagePassesItem($item)) {
+                    if ($item->quality > 0) {
+                        if (!$this->isSulfurasItem($item)) {
+                            $item->quality = $item->quality - 1;
+                        }
+                    }
+                } else {
+                    $item->quality = $item->quality - $item->quality;
+                }
+            } else {
+                if ($item->quality < 50) {
+                    $item->quality = $item->quality + 1;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param  Item $item
+     * @return bool
+     */
+    private function isSpecialItem(Item $item)
+    {
+        return $this->isBackstagePassesItem($item)
+                || $this->isAgedBrieItem($item)
+                || $this->isSulfurasItem($item);
+    }
+
+    /**
+     * @param Item $item
+     * @return bool
+     */
+    private function isAgedBrieItem(Item $item)
+    {
+        return $item->name == "Aged Brie";
+    }
+
+    /**
+     * @param Item $item
+     * @return bool
+     */
+    private function isBackstagePassesItem(Item $item)
+    {
+        return $item->name == "Backstage passes to a TAFKAL80ETC concert";
+    }
+
+    /**
+     * @param Item $item
+     * @return bool
+     */
+    private function isSulfurasItem(Item $item)
+    {
+        return $item->name == "Sulfuras, Hand of Ragnaros";
     }
 }
